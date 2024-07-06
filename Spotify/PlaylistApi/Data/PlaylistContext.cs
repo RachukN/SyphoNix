75c2cb0529650ef3df02507487ddead9684cd4bf
RachukN <120932253+RachukN@users.noreply.github.com>
2024-07-06 12:42:38 +0300

    conected localserver and add registration

---
+++ Spotify/PlaylistApi/Data/PlaylistContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlaylistApi.Models;
+
+namespace PlaylistApi.Data
+{
+    // Data/PlaylistContext.cs
+    public class PlaylistContext : DbContext
+    {
+        public PlaylistContext(DbContextOptions<PlaylistContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+    }
+
+}
---
+++ Spotify/Spotify/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Spotify.Models;
+
+namespace Spotify.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
