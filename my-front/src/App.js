ac1c39a148a34544c1502aa6f43fdc4dcdfefd09
RachukN <120932253+RachukN@users.noreply.github.com>
2024-07-06 11:36:19 +0300

    add localserver and add registering users

---
+++ my-front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spotify Clone Registration</h1>
+      <Register />
+    </div>
+  );
+}
+
+export default App;
